a71aa77f24f7a8952b8a08962948519114861da1
vnrai22 <39534363+vnrai22@users.noreply.github.com>
2018-06-05 05:38:45 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mysql = require('mysql');
 
 const app = express();
 //external bodyparser middleware
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
+//databse connection
+/*
+const db = mysql.createConnection({
+	host: 'localhost',
+	user: 'username',
+	password: 'pass',
+	database: 'db_name'
+});
 
+db.connect(function(err) {
+	if(err){
+		throw err;
+	}
+	console.log('mySql connected');
+});
+*/
 //use Static pages
 //app.use(express.static('public'));
 
